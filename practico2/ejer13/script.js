3f6e0e72ff80d535784585184250c7e8ff93c8bd
JoaquinC2000 <joaquincenicero2000@hotmail.com>
2024-09-20 19:19:00 -0300

    actualizacion 6.0

--- practico2/ejer13/script.js
+++ practico2/ejer13/script.js
@@ -1,52 +1,128 @@
-let currentNumber = '';
-let previousNumber = '';
-let operation = '';
+const pantalla = document.querySelector(".pantalla");
+const pantalla1 = document.querySelector(".pantalla1");
+const botones = document.querySelectorAll(".btn");
+let igual = false; 
 
-function appendNumber(num){
-    currentNumber += num;
+botones.forEach((boton) => {
+    boton.addEventListener("click", () => {
+        const botonApretado = boton.textContent;
 
-    document.getElementById("resultado").value = currentNumber;
-}
+        if(boton.id === "c"){
+            pantalla.textContent = "0";
+            pantalla1.textContent = " ";
+            return;
+        }
+        if(boton.id === "ce"){
+            pantalla.textContent = "0";
+            if(igual === true){
+                pantalla1.textContent = " ";
+            }
+            igual = false;
+            return;
+        }
+        if(boton.id === "borrar" || pantalla.textContent === "Error!"){
+            if(pantalla.textContent.length === 1){
+                pantalla.textContent = "0";
+            }else{
+                pantalla.textContent = pantalla.textContent.slice(0, -1);
+            }
+            if(igual){
+                pantalla1.textContent = " "
+            }
+            igual = false;
+            return;
+        }
 
-// setOperacion(op){
+        if(boton.id === "suma"){
+            if(!igual){
+                pantalla1.textContent = eval(pantalla1.textContent + pantalla.textContent) + "+";
+            }else{
+                pantalla1.textContent = pantalla.textContent + "+"; 
+            }
+            pantalla.textContent = "0";
+            return;
+        }
 
+        if(boton.id === "resta"){
+            if(!igual){
+                pantalla1.textContent = eval(pantalla1.textContent + pantalla.textContent) + "-";
+            }else{
+                pantalla1.textContent = pantalla.textContent + "-"; 
+            }
+            pantalla.textContent = "0";
+            return;
+        }
+
+        if(boton.id === "igual"){
+            try {
+                igual = true;
+                let num = pantalla.textContent;
+                pantalla.textContent = eval(pantalla1.textContent + pantalla.textContent);
+                pantalla1.textContent = pantalla1.textContent + num + "=";
+            } catch {
+                pantalla.textContent = "Error!";
+            }
+            return;
+        }
+ 
+        if (pantalla.textContent === "0" || pantalla.textContent === "Error!") {
+            pantalla.textContent = botonApretado;
+        } else if(igual){
+            pantalla1.textContent = "";
+            pantalla.textContent = botonApretado;
+            igual = false;
+        }else {
+        pantalla.textContent += botonApretado;
+        }
+    });
+});
+
+// function appendNumber(num){
+//     if(pantalla.textContent === "0"){
+//         pantalla.textContent = num;
+//     }else{
+//         pantalla.textContent += num;
+//     }
 // }
 
-function clearScreen(){
-    currentNumber = '';
-    previousNumber = '';
-    operation = '';
-    document.getElementById("resultado").value = '';
-}
+// // setOperacion(op){
 
-function calcular(){
-    let resultado;
-    const prev = parseFloat(previousNumber);
-    const curr = parseFloat(currentNumber);
+// // }
 
-    if(isNaN(prev) || isNaN(curr)) return;
+// function clearScreen(){
+//     currentNumber = '';
+//     previousNumber = '';
+//     operation = '';
+//     document.getElementById("resultado").value = '';
+// }
 
-    switch(operation){
-        case '+':
-            resultado = prev + curr;
-            break;
-        case '-':
-            resultado = curr - prev;
-            break;
-        case '*':
-            resultado = prev * curr;
-            break;
-        case '/':
-            resultado = curr / curr;
-            break;
-        default:
-            return;
-    }
+// function calcular(){
+//     let resultado;
+//     const prev = parseFloat(previousNumber);
+//     const curr = parseFloat(currentNumber);
 
-    currentNumber = resultado.toString();
-    operation = '';
-    previousNumber = '';
-    historial = '';
-    actualizarPantalla();
-}
+//     if(isNaN(prev) || isNaN(curr)) return;
 
+//     switch(operation){
+//         case '+':
+//             resultado = prev + curr;
+//             break;
+//         case '-':
+//             resultado = curr - prev;
+//             break;
+//         case '*':
+//             resultado = prev * curr;
+//             break;
+//         case '/':
+//             resultado = curr / curr;
+//             break;
+//         default:
+//             return;
+//     }
+
+//     currentNumber = resultado.toString();
+//     operation = '';
+//     previousNumber = '';
+//     historial = '';
+//     actualizarPantalla();
+// }
