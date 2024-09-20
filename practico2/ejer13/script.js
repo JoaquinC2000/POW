1991bf15e3e94261a2d39af347fff769ae71836c
JoaquinC2000 <joaquincenicero2000@hotmail.com>
2024-09-20 15:22:23 -0300

    actualizacion 5.0

--- practico2/ejer13/script.js
+++ practico2/ejer13/script.js
@@ -1,4 +1,4 @@
-let currentNumber = '0';
+let currentNumber = '';
 let previousNumber = '';
 let operation = '';
 
@@ -8,17 +8,45 @@ function appendNumber(num){
     document.getElementById("resultado").value = currentNumber;
 }
 
-setOperacion(op){
+// setOperacion(op){
 
-}
+// }
 
 function clearScreen(){
-    currentNumber = '0';
+    currentNumber = '';
     previousNumber = '';
     operation = '';
     document.getElementById("resultado").value = '';
 }
 
 function calcular(){
+    let resultado;
+    const prev = parseFloat(previousNumber);
+    const curr = parseFloat(currentNumber);
 
+    if(isNaN(prev) || isNaN(curr)) return;
+
+    switch(operation){
+        case '+':
+            resultado = prev + curr;
+            break;
+        case '-':
+            resultado = curr - prev;
+            break;
+        case '*':
+            resultado = prev * curr;
+            break;
+        case '/':
+            resultado = curr / curr;
+            break;
+        default:
+            return;
+    }
+
+    currentNumber = resultado.toString();
+    operation = '';
+    previousNumber = '';
+    historial = '';
+    actualizarPantalla();
 }
+
