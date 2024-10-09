53eca5a293bb2a615902ff185eac078dcbdce836
JoaquinC2000 <joaquincenicero2000@hotmail.com>
2024-10-09 17:40:27 -0300

    actualizacion 10.0

--- prac/cositas/sidbar(barra-lateral)/script.js
+++ prac/cositas/sidbar(barra-lateral)/script.js
@@ -1,4 +1,34 @@
+const boton_alternar = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+function toggleSidebar() {
+    sidebar.classList.toggle('close');
+    boton_alternar.classList.toggle('rotar')
+
+    closeAllSubMenus()
+}
+
 function toggleSubMenu(button){
+    //button: contiene la info del boton que se hizo clic.
+    //verifica si el subMenu actual, está abierto
+    if(!button.nextElementSibling.classList.contains('show')){
+        closeAllSubMenus()
+    }
+
     button.nextElementSibling.classList.toggle('show');
     button.classList.toggle('rotar');
+
+    if(sidebar.classList.contains('close')){
+        sidebar.classList.toggle('close');
+        boton_alternar.classList.toggle('rotar');
+    }
+}
+
+function closeAllSubMenus(){
+    //Se obtiene una matriz con todos los menus desplegables
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+
+        ul.classList.remove('show');
+        ul.previousElementSibling.classList.remove('rotar');
+    })
 }
