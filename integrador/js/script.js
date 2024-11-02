6e14ef54f5e936df9a83a401f739c1d8c22ecc39
JoaquinC2000 <joaquincenicero2000@hotmail.com>
2024-11-02 14:52:47 -0300

    actualizacion 12.0

--- integrador/js/script.js
+++ integrador/js/script.js
@@ -32,3 +32,17 @@ function closeAllSubMenus(){
         ul.previousElementSibling.classList.remove('rotar');
     })
 }
+
+
+
+const cloud = document.getElementById('cloud');
+const barralateral = document.querySelector(".barra-lateral")
+const spans = document.querySelectorAll("span");
+
+cloud.addEventListener('click', () => {
+    barralateral.classList.toggle("mini-barra-lateral");
+    
+    spans.forEach(span => {
+        span.classList.toggle("oculto");
+    })
+});
